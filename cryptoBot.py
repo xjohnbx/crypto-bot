7c62ec8edf65b240e04adfeba5c11734771832ab
John Brechon <john.brechon@allscripts.com>
2021-06-22 16:35:43 -0500

    First commit

---
+++ cryptoBot.py
@@ -0,0 +1,16 @@
+# This is a python progarm that interacts with the Coinbase Pro APIs
+
+
+# We need to generate an api account via Coinbase pro
+# This will give us:
+	# Key Secret Passphrase
+	
+import json
+import requests
+from pprint import pprint
+
+def main()
+	
+	
+main()
+
